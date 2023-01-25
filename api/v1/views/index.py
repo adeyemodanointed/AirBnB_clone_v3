ee34a2f7c957e60d5f258cfdc2b977dc2fd5b8e5
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-01-25 09:48:59 +0000

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,24 @@ def status():
     """Return status of application
     """
     return jsonify({'status': 'OK'})
+
+
+def stats():
+    """Retrieve count of objects in storage
+    """
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    json_dict = {}
+
+    for name, cls in classes.items():
+        json_dict.update({name: storage.count(cls)})
+
+    return jsonify(json_dict)
